11e64cb91bf1192926ce8aff8a9618a20bb575a6
jkxruby <jkxruby@gmail.com>
2017-08-10 09:55:43 +0800

    add views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,8 +15,9 @@ def create
   @group = Group.find(params[:group_id])
   @post = Post.new(post_params)
   @post.group = @group
+  @post.user = current_user
   if @post.save
-    redirect_to group_path(group)
+    redirect_to group_path(@group)
   else
     render :new
   end
@@ -24,8 +25,8 @@ end
 
 private
 def post_params
-  params.require(:post).permit(:group_id, :content)
-end 
+  params.require(:post).permit(:content)
+end
 
 
 end
